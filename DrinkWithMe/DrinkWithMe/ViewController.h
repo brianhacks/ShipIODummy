568b9bb2235ecd4cd86b5cdd24e75ec13e32d4a3
ndbinh99 <iphone.cntt@gmail.com>
2015-04-18 15:14:26 +0700

    init a dummy project

---
+++ DrinkWithMe/DrinkWithMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrinkWithMe
+//
+//  Created by Binh Nguyen on 4/18/15.
+//  Copyright (c) 2015 mobytelab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
